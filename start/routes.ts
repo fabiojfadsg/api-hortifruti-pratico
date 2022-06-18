2d856abc500586fd2571059d218933cf8af30da0
Fabio Santos <fabio.santos@i-value.com.br>
2022-06-18 03:49:37 -0300

    Aula 6 middleware

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,27 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
 import User from 'App/Models/user'
 
 Route.get('/', async () => {
   User.create({ email: 'admin@email.com', password: '123456', tipo: 'admin' },)
 })
+
+
+Route.post('/token', async ({request, response  , auth}: HttpContextContract) => {
+  const email = request.input('email');
+  const password = request.input('password');
+
+  const user = await User.findBy('email', email);
+
+  if (user == null) {
+    return response.notFound('User not found');
+  }
+
+  const token = await auth.use('api').attempt(email, password);
+  return response.ok(token);
+})
+
+Route .get('/auth', async ({ response }: HttpContextContract) => { 
+  return response.ok("Somente usuários autenticados podem acessar essa rota");
+
+}).middleware("auth");
