4f633323f9519de4928afed71eba6db990f18923
Fabio Santos <fabio.santos@i-value.com.br>
2022-06-19 01:07:28 -0300

    22. Criando a rota /auth/me e testando

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+//Login para os 3 tipos de usuários
+Route.post('/login', 'AuthController.login');
+Route.post('logout', 'AuthController.logout');
+
+Route.group(() => {
+  Route.get('auth/me', 'AuthController.me');
+}).middleware('auth');
+
 Route.get('/', async () => {
   return { hortifruti: 'prático' }
 })
